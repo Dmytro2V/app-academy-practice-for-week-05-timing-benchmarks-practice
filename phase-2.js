520ac31d413c9acf411bdf2936015f3d83f8cebd
Dmytro2V <op2vin@gmail.com>
2022-10-10 17:10:33 +0300

    done

--- phase-2.js
+++ phase-2.js
@@ -3,13 +3,26 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
-
+  let arr =[];
+  let n = 0  
+  for ( let i = 1; i <= 10; i++){
+   // let n +=
+     arr.push(addNums(i * increment))
+     
+  }
+  return arr;
 }
 
 // Runs `addManyNums` in 10 increasing increments
+
 function addManyNums10(increment) {
   // Fill this in
-
+  
+  let arr =[];
+  for ( let i = 1; i <= 10; i++){
+     arr.push(addManyNums(i * increment))
+  }
+  return arr;
 }
-
+console.log(addNums10(1));
 module.exports = [addNums10, addManyNums10];
--- phase-3.js
+++ phase-3.js
@@ -1,10 +1,21 @@
 const [addNums, addManyNums] = require("./phase-1");
 
-function addNums10Timing(increment) {
+function addNums100Timing(increment) {
   // Copy your `addNums10` code here
   // Then, add timing code
 
   // Your code here
+  let arr =[];
+  let beg = Date.now()
+  console.time() 
+  for ( let i = 1; i <= 100; i++){
+   // let n +=
+     arr.push(addNums(i * increment))
+     //console.timeLog()     
+     console.log(Date.now()- beg);
+  };
+  console.timeEnd()
+  return arr;
 
 }
 
@@ -14,13 +25,22 @@ function addManyNums10Timing(increment) {
 // Then, add timing code
 
   // Your code here
-
+  let arr =[];
+  console.time() 
+  let beg = Date.now()
+  for ( let i = 1; i <= 10; i++){
+     arr.push(addManyNums(i * increment))
+     //console.timeLog()   
+     console.log(Date.now()- beg);  
+  }
+  console.timeEnd()
+  return arr;
 }
 
 
 n = 1000000
 console.log(`addNums(${n}): `);
-addNums10Timing(1000000);
+addNums100Timing(1000000);
 
 console.log("\n***********\n");
 
